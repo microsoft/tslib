2c3978f550516481d05acd8c03a0836ef6be8000
Ron Buckton <rbuckton@microsoft.com>
2016-06-13 12:24:53 -0700

    Added __generator helper.

--- tslib.js
+++ tslib.js
@@ -19,6 +19,7 @@ var __decorate;
 var __param;
 var __metadata;
 var __awaiter;
+var __generator;
 (function (factory) {
     var root = typeof global === "object" ? global : typeof self === "object" ? self : typeof this === "object" ? this : {};
     if (typeof System === "object" && typeof System.register === "function") {
@@ -80,10 +81,107 @@ var __awaiter;
         });
     };
 
+    __generator = function (body) {
+        var done, finallyStack, executing, yieldStar, trys = [], state = { label: 0, trys: trys };
+        function step(op) {
+            if (executing) throw new TypeError("Generator is already executing.");
+            while (true) {
+                if (done) {
+                    switch (op[0]) {
+                        case 0 /*next*/: return { value: void 0, done: true };
+                        case 6 /*catch*/:
+                        case 1 /*throw*/: throw op[1];
+                        case 2 /*return*/: return { value: op[1], done: true };
+                    }
+                }
+                try {
+                    executing = true;
+                    if (yieldStar) {
+                        var verb = yieldStar[op[0] === 2 ? "return" : op[0] === 1 ? "throw" : "next"];
+                        if (verb) {
+                            var yielded = verb.call(yieldStar, op[1]);
+                            if (!yielded.done) return { value: yielded.value, done: false };
+                            op = [0 /*next*/, yielded.value];
+                        }
+
+                        yieldStar = void 0;
+                        continue;
+                    }
+
+                    switch (op[0]) {
+                        case 0 /*next*/:
+                            state.sent = function() { return op[1]; };
+                            break;
+
+                        case 1 /*throw*/:
+                            state.sent = function() { throw op[1]; };
+                            break;
+
+                        case 4 /*yield*/:
+                            state.label++;
+                            return { value: op[1], done: false };
+
+                        case 5 /*yieldstar*/:
+                            state.label++;
+                            yieldStar = op[1];
+                            op = [0 /*next*/, void 0];
+                            continue;
+
+                        case 7 /*endfinally*/:
+                            op = finallyStack.pop();
+                            trys.pop();
+                            continue;
+
+                        default:
+                            var rgn = trys.length && trys[trys.length - 1];
+                            if (!rgn && (op[0] === 6 /*catch*/ || op[0] === 1 /*throw*/ || op[0] === 2 /*return*/)) {
+                                done = true;
+                                finallyStack = void 0;
+                                continue;
+                            }
+                            else if (op[0] === 3 /*break*/ && (!rgn || (op[1] > rgn[0] && op[1] < rgn[3]))) {
+                                state.label = op[1];
+                            }
+                            else if (op[0] === 6 /*catch*/ && rgn && state.label < rgn[1]) {
+                                state.error = op[1];
+                                state.label = rgn[1];
+                            }
+                            else if (rgn && state.label < rgn[2]) {
+                                (finallyStack || []).push(op);
+                                state.label = rgn[2];
+                            }
+                            else {
+                                if (rgn[2]) finallyStack.pop();
+                                trys.pop();
+                                continue;
+                            }
+                    }
+
+                    op = body(state);
+                }
+                catch (e) {
+                    op = [6 /*catch*/, e];
+                    yieldStar = void 0;
+                }
+                finally {
+                    executing = false;
+                    verb = yielded = void 0;
+                }
+            }
+        }
+        var g = {
+            next: function (v) { return step([0 /*next*/, v]); },
+            "throw": function (v) { return step([1 /*throw*/, v]); },
+            "return": function (v) { return step([2 /*return*/, v]); }
+        };
+        return g;
+    };
+
     exporter("__extends", __extends);
     exporter("__assign", __assign);
     exporter("__decorate", __decorate);
     exporter("__param", __param);
     exporter("__metadata", __metadata);
     exporter("__awaiter", __awaiter);
+    exporter("__generator", __generator);
 });
