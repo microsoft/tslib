a5b18fe772ecfa36a118230e7bc012cd1d245fad
Ron Buckton <rbuckton@microsoft.com>
2016-09-09 13:27:19 -0700

    Adds __generator helper

--- tslib.js
+++ tslib.js
@@ -19,6 +19,7 @@ var __decorate;
 var __param;
 var __metadata;
 var __awaiter;
+var __generator;
 (function (factory) {
     var root = typeof global === "object" ? global : typeof self === "object" ? self : typeof this === "object" ? this : {};
     if (typeof System === "object" && typeof System.register === "function") {
@@ -80,10 +81,49 @@ var __awaiter;
         });
     };
 
+    __generator = function (thisArg, body) {
+        var _ = { label: 0, sent: function() { if (sent[0] === 1) throw sent[1]; return sent[1]; }, trys: [], stack: [] }, sent, f;
+        function step(op) {
+            if (f) throw new TypeError("Generator is already executing.");
+            while (1) {
+                if (_.done) switch (op[0]) {
+                    case 0: return { value: void 0, done: true };
+                    case 1: case 6: throw op[1];
+                    case 2: return { value: op[1], done: true };
+                }
+                try {
+                    switch (f = 1, op[0]) {
+                        case 0: case 1: sent = op; break;
+                        case 4: return _.label++, { value: op[1], done: false };
+                        case 7: op = _.stack.pop(), _.trys.pop(); continue;
+                        default:
+                            var r = _.trys.length > 0 && _.trys[_.trys.length - 1];
+                            if (!r && (op[0] === 6 || op[0] === 2)) { _.done = 1; continue; }
+                            if (op[0] === 3 && (!r || (op[1] > r[0] && op[1] < r[3]))) { _.label = op[1]; break; }
+                            if (op[0] === 6 && _.label < r[1]) { _.label = r[1], sent = op; break; }
+                            if (r && _.label < r[2]) { _.label = r[2], _.stack.push(op); break; }
+                            if (r[2]) { _.stack.pop(); }
+                            _.trys.pop();
+                            continue;
+                    }
+                    op = body.call(thisArg, _);
+                }
+                catch (e) { op = [6, e]; }
+                finally { f = 0, sent = void 0; }
+            }
+        }
+        return {
+            next: function (v) { return step([0, v]); },
+            "throw": function (v) { return step([1, v]); },
+            "return": function (v) { return step([2, v]); }
+        };
+    };
+
     exporter("__extends", __extends);
     exporter("__assign", __assign);
     exporter("__decorate", __decorate);
     exporter("__param", __param);
     exporter("__metadata", __metadata);
     exporter("__awaiter", __awaiter);
+    exporter("__generator", __generator);
 });
