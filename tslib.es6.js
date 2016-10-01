2af77d76c71a1bb6e932aa405b033388a8b035c4
Frank Wallis <fwallis@outlook.com>
2016-10-01 09:35:44 +0100

    Add __extends helper to es6 source

--- tslib.es6.js
+++ tslib.es6.js
@@ -21,6 +21,12 @@ export const __assign = Object.assign || function (t) {
     return t;
 };
 
+export const __extends = function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+
 export const __decorate = function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
