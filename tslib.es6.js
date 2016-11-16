3e929fa5e950f1725c6c3ac2b6ee564d324bfb55
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-11-16 09:59:59 -0800

    Update __rest emit to handle symbols

--- tslib.es6.js
+++ tslib.es6.js
@@ -23,8 +23,11 @@ export const __assign = Object.assign || function (t) {
 
 export const __rest = function (s, e) {
     var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && !e.indexOf(p))
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
         t[p] = s[p];
+    if (typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
     return t;
 };
 
