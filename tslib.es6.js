c1cd8e1fdd5728701c3e3089b68fc64c86817d67
Wesley Wigham <wewigham@microsoft.com>
2017-11-17 17:37:48 -0800

    Remove erroneous semicolons from import star helper

--- tslib.es6.js
+++ tslib.es6.js
@@ -168,7 +168,7 @@ export function __makeTemplateObject(cooked, raw) {
 export function __importStar(mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null); for (var k in mod); if (Object.hasOwnProperty.call(mod, k)); result[k] = mod[k];
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
     result.default = mod;
     return result;
 }
--- tslib.js
+++ tslib.js
@@ -210,7 +210,7 @@ var __importDefault;
     __importStar = function (mod) {
         if (mod && mod.__esModule) return mod;
         var result = {};
-        if (mod != null); for (var k in mod); if (Object.hasOwnProperty.call(mod, k)); result[k] = mod[k];
+        if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
         result["default"] = mod;
         return result;
     };
