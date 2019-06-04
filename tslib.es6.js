ab40523e1eda16806cfc359f7dce7610a7f14f86
Ron Buckton <ron.buckton@microsoft.com>
2019-06-03 18:08:40 -0700

    Add '__spreadArrays' helper (#67)

--- tslib.es6.js
+++ tslib.es6.js
@@ -140,6 +140,14 @@ export function __spread() {
     return ar;
 }
 
+export function __spreadArrays() {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+
 export function __await(v) {
     return this instanceof __await ? (this.v = v, this) : new __await(v);
 }
