18b0b94e90d748fe1f8c0bcb12c3ddc20bbdcff6
Ron Buckton <ron.buckton@microsoft.com>
2019-08-06 10:53:00 -0700

    Editorial changes from PR suggestions

--- tslib.js
+++ tslib.js
@@ -153,7 +153,7 @@ var __importDefault;
                 return { value: o && o[i++], done: !o };
             }
         };
-        throw new TypeError(s ? "Object not iterable." : "Symbol.iterator is not defined.");
+        throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
     };
 
     __read = function (o, n) {
