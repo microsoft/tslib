74f1ff794985b33657b599b611e82937e3074617
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2020-04-21 16:54:49 -0700

    Don't be so reserved about a relicense.

--- tslib.es6.js
+++ tslib.es6.js
@@ -1,5 +1,5 @@
 /*! *****************************************************************************
-Copyright (c) Microsoft Corporation. All rights reserved.
+Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
 purpose with or without fee is hereby granted.
