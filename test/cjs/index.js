fec21972b22f3b1119bb27b4baf1e9978b8ef95d
Orta <git@orta.io>
2020-09-29 15:20:32 -0400

    Adds CI to verify

---
+++ test/cjs/index.js
@@ -0,0 +1,7 @@
+const { __awaiter } = require("tslib");
+
+const printSomething = (textToPrint) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(`Awaiter: ${textToPrint}`);
+});
+
+printSomething("Hi!")
---
+++ test/esm/index.js
@@ -0,0 +1,7 @@
+import { __awaiter } from "tslib";
+
+export const printSomething = (textToPrint) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(`Inside ${textToPrint}`);
+});
+
+printSomething("Hi!")
