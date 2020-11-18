be654a5062c8eb051f1e72251c8e37f8ce6581e1
Orta <git@orta.io>
2020-11-18 11:33:09 -0500

    Ensure that the modules exports are validated in tests

--- test/validateModuleExportsMatchCommonJS/index.js
+++ test/validateModuleExportsMatchCommonJS/index.js
@@ -1,11 +1,5 @@
-// When on node 14, it validates that all of the commonjs exports
+// This file can only run on node 14+, it validates that all of the commonjs exports
 // are correctly re-exported for es modules importers.
-
-const nodeMajor = Number(process.version.split(".")[0].slice(1))
-if (nodeMajor < 14) {
-  console.log("Skipping because node does not support module exports.")
-  process.exit(0)
-}
 
 // ES Modules import via the ./modules folder
 import * as esTSLib from "../../modules/index.js"
