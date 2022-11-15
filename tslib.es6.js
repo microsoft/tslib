f26d52113a3baaf5b72117c419800b049c29116d
Tomasz Lenarcik <apendua@gmail.com>
2022-11-15 23:44:18 +0100

    Fix asyncDelegator reporting done too early
    See: https://github.com/microsoft/TypeScript/issues/45400

--- tslib.es6.js
+++ tslib.es6.js
@@ -195,7 +195,7 @@ export function __asyncGenerator(thisArg, _arguments, generator) {
 export function __asyncDelegator(o) {
     var i, p;
     return i = {}, verb("next"), verb("throw", function (e) { throw e; }), verb("return"), i[Symbol.iterator] = function () { return this; }, i;
-    function verb(n, f) { i[n] = o[n] ? function (v) { return (p = !p) ? { value: __await(o[n](v)), done: n === "return" } : f ? f(v) : v; } : f; }
+    function verb(n, f) { i[n] = o[n] ? function (v) { return (p = !p) ? { value: __await(o[n](v)), done: false } : f ? f(v) : v; } : f; }
 }
 
 export function __asyncValues(o) {
