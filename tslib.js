27b219209a4d35308e9dd6a533e5169a981d2614
Andrew Branch <andrew@wheream.io>
2024-09-24 16:04:07 -0700

    Little optimizations

--- tslib.js
+++ tslib.js
@@ -398,8 +398,8 @@ var __rewriteRelativeImportExtension;
 
     __rewriteRelativeImportExtension = function (path, preserveJsx) {
         if (typeof path === "string" && /^\.\.?\//.test(path)) {
-            return path.replace(/\.(tsx)$|(\.d)?(\.[^./]+?)?\.([cm])?ts$/i, function (m, tsx, d, ext, cm) {
-                return tsx ? preserveJsx ? ".jsx" : ".js" : d && (ext && !cm || !ext) ? m : ((d || "") + (ext || "") + "." + (cm || "").toLowerCase() + "js");
+            return path.replace(/\.(tsx)$|((?:\.d)?)((?:\.[^./]+?)?)\.([cm]?)ts$/i, function (m, tsx, d, ext, cm) {
+                return tsx ? preserveJsx ? ".jsx" : ".js" : d && (!ext || !cm) ? m : (d + ext + "." + cm.toLowerCase() + "js");
             });
         }
         return path;
