e828dac33c7721a8ab16a37200dfb6101958d351
Andrew Branch <andrew@wheream.io>
2024-09-24 09:38:27 -0700

    Shorten by one line

--- tslib.es6.js
+++ tslib.es6.js
@@ -357,10 +357,9 @@ export function __rewriteRelativeImportExtension(path, preserveJsx) {
         if (extMatch) {
             var ext = extMatch[0].toLowerCase();
             var dot = path.lastIndexOf(".", path.length - (ext.length + 1));
-            if (dot >= 0 && (path.substring(dot - 2, dot).toLowerCase() === ".d" || path.substring(dot, dot + 2).toLowerCase() === ".d")) {
-                return path;
+            if (dot < 0 || !(path.substring(dot - 2, dot).toLowerCase() === ".d" || path.substring(dot, dot + 2).toLowerCase() === ".d")) {
+                return path.substring(0, path.length - ext.length) + (ext === ".mts" ? ".mjs" : ext === ".cts" ? ".cjs" : ".js");
             }
-            return path.substring(0, path.length - ext.length) + (ext === ".mts" ? ".mjs" : ext === ".cts" ? ".cjs" : ".js");
         }
     }
     return path;
